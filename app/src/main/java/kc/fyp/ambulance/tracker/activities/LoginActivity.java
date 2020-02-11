025728fe500d7726a14bb565b4b8642dc9a644bf
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-02-11 17:14:32 +0500

    Project Started

---
+++ app/src/main/java/kc/fyp/ambulance/tracker/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package kc.fyp.ambulance.tracker.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kc.fyp.ambulance.tracker.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/kc/fyp/ambulance/tracker/activities/SplashActivity.java
@@ -0,0 +1,82 @@
+package kc.fyp.ambulance.tracker.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import kc.fyp.ambulance.tracker.R;
+
+public class SplashActivity extends Activity {
+    private boolean isFirstAnimation = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Animation hold = AnimationUtils.loadAnimation(this, R.anim.hold);
+
+        final Animation translateScale = AnimationUtils.loadAnimation(this, R.anim.translate_scale);
+
+        final ImageView imageView = findViewById(R.id.header_icon);
+
+        translateScale.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (!isFirstAnimation) {
+                    imageView.clearAnimation();
+//                    Session session = new Session(SplashActivity.this);
+//                    User user = session.getUser();
+//                    if (user == null) {
+//                        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+//                        startActivity(intent);
+//                    } else {
+//                        if (user.getRoll() == 0) {
+//                            Intent intent = new Intent(SplashActivity.this, Dashboard.class);
+//                            startActivity(intent);
+//                        } else {
+//                            Intent intent = new Intent(SplashActivity.this, ProviderDashboard.class);
+//                            startActivity(intent);
+//                        }
+//                        finish();
+//                    }
+                    isFirstAnimation = true;
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        hold.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.clearAnimation();
+                imageView.startAnimation(translateScale);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        imageView.startAnimation(hold);
+
+
+    }
+}
